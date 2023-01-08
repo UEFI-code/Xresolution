fcbd3ee249ab81389982893baa71eda746c4d3a2
UEFI-code <1092700291@qq.com>
2023-01-08 15:45:57 +0800

    For Small VRAM

--- model.py
+++ model.py
@@ -7,15 +7,15 @@ class myModel(nn.Module):
     def __init__(self):
         super(myModel, self).__init__()
         self.relu = nn.ReLU()
-        self.up1 = bad_graph_transformer.BadGraphTransformerUp(3, 64, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(64), activation=self.relu, dropout=None)
-        self.up2 = bad_graph_transformer.BadGraphTransformerUp(64, 128, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(128), activation=self.relu, dropout=None)
-        self.up3 = bad_graph_transformer.BadGraphTransformerUp(128, 128, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(128), activation=self.relu, dropout=None)
-        self.down = bad_graph_transformer.BadGraphTransformerDown(128, 3, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(3), activation=self.relu, dropout=None)
+        self.up1 = bad_graph_transformer.BadGraphTransformerUp(3, 8, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(8), activation=self.relu, dropout=None)
+        self.up2 = bad_graph_transformer.BadGraphTransformerUp(8, 8, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(8), activation=self.relu, dropout=None)
+        self.up3 = bad_graph_transformer.BadGraphTransformerUp(8, 8, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(8), activation=self.relu, dropout=None)
+        self.down = bad_graph_transformer.BadGraphTransformerDown(8, 3, 3, stride=2, padding=0, normalization=nn.BatchNorm2d(3), activation=self.relu, dropout=None)
     
     def forward(self, x):
         x = self.up1(x)
         x = self.up2(x)
-        x = self.up3(x)
+        #x = self.up3(x)
         x = self.down(x)
         return x
     
