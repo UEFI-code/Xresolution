ba1331603799fe717c76f5cd5da04c092dc1e2d9
UEFI-code <1092700291@qq.com>
2023-01-08 10:44:39 +0800

    Bug Fix

--- bad_graph_transformer.py
+++ bad_graph_transformer.py
@@ -21,7 +21,7 @@ class BadGraphTransformerDown(nn.Module):
             b = self.activation(b)
             c = self.activation(c)
         x = a * b + c
-        if dropout is not None:
+        if self.dropout is not None:
             x = self.dropout(x)
         if self.normalization is not None:
             x = self.normalization(x)
