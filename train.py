23d7d043ed1e87538d2398abb51cae187882cd11
UEFI-code <1092700291@qq.com>
2023-01-08 15:57:38 +0800

    Bug Fix

--- train.py
+++ train.py
@@ -5,7 +5,8 @@ import dataset
 import model
 import torch.optim as optim
 import tqdm
-
+import cv2
+import os
 # Create the model
 myModel = model.myModel()
 # Create the optimizer
@@ -13,19 +14,19 @@ optimizer = optim.Adam(myModel.parameters(), lr=0.001)
 # Create the loss function
 loss_fn = nn.MSELoss()
 # Create the dataset
-myDataset = dataset.ImgDataset('Download/images')
+myDataset = dataset.ImgDataset('/hdd2/uefi/GANData/train', True)
 # Train the model
 
-def train(batchsize = 16, epoch = 10, device = 'cpu', show = False, restore = False):
+def train(batchsize = 16, epoch = 10, device = 'cpu', show = False, restore = True):
     myModel.to(device)
     myModel.train()
-    if restore:
+    if restore and os.path.exists('model.pth'):
         print("Restoring model...")
         myModel.load_state_dict(torch.load('model.pth'))
     for i in range(epoch):
         for j in tqdm.tqdm(range(len(myDataset) // batchsize)):
             batchS = myDataset.makeBatch(batchsize, resolution=(256, 256)).to(device) / 255
-            batchT = myDataset.makeBatch(batchsize, resolution=(512, 512)).to(device) / 255
+            batchT = myDataset.makeBatch(batchsize, resolution=(513, 513)).to(device) / 255
             y = myModel(batchS)
             loss = loss_fn(y, batchT)
             optimizer.zero_grad()
@@ -41,4 +42,4 @@ def train(batchsize = 16, epoch = 10, device = 'cpu', show = False, restore = Fa
         print("Epoch: " + str(i) + " Loss: " + str(loss.item()))
         torch.save(myModel.state_dict(), 'model.pth')
 
-train(batchsize = 16, epoch = 10, device = 'cuda', show = True)
+train(batchsize = 50, epoch = 10, device = 'cuda', show = True)
